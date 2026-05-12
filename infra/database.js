379cc8709ff8f038fcc85cb5154599da5cdf69db
Bruno Lopes <168757702+fbgrangeiro@users.noreply.github.com>
2026-05-12 00:59:04 +0000

    refact: variaveis

--- infra/database.js
+++ infra/database.js
@@ -2,11 +2,11 @@ import { Client } from "pg";
 
 async function query(queryObject) {
   const client = new Client({
-    user: "postgres",
-    host: "localhost",
-    database: "postgres",
-    password: "local_password",
-    port: 5432,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
   });
   await client.connect();
   const result = await client.query(queryObject);
